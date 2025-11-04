fdb4a859501e934b67a111783389b05e249eaa5a
방혜준 <kkngrs2@gmail.com>
2025-11-04 14:26:08 +0900

    [Bronze I] Title: 단어 공부, Time: 392 ms, Memory: 29012 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,37 +3,27 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String word = sc.next();
+        String word = sc.next().toUpperCase();
         int[] lettersCount = new int[26];
 
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            if ('a' <= ch && ch <= 'z') {
-                lettersCount[ch - 'a']++;
-            } else if ('A' <= ch && ch <= 'Z') {
+            if ('A' <= ch && ch <= 'Z') {
                 lettersCount[ch - 'A']++;
             }
         }
 
-        int max = 0;
-        int maxIdx = -1;
+        int maxCount = -1;
+        char maxAlphabet = '?';
         for (int i = 0; i < 26; i++) {
-            if (lettersCount[i] > max) {
-                max = lettersCount[i];
-                maxIdx = i;
-            }
-        }
-        int maxCount = 0;
-        for (int i = 0; i < 26; i++) {
-            if (lettersCount[i] == max) {
-                maxCount++;
+            if (lettersCount[i] > maxCount) {
+                maxCount = lettersCount[i];
+                maxAlphabet = (char)(i + 'A');
+            } else if (lettersCount[i] == maxCount) {
+                maxAlphabet = '?';
             }
         }
 
-        if (maxCount > 1) {
-            System.out.println("?");
-        } else {
-            System.out.println((char)(maxIdx + 'A'));
-        }
+        System.out.println(maxAlphabet);
     }
 }
