22da9490621cc5519a3c8d2636e3d52b2a7f2a63
방혜준 <kkngrs2@gmail.com>
2025-11-20 20:38:03 +0900

    [Bronze I] Title: 유레카 이론, Time: 252 ms, Memory: 20592 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,36 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Main {
+
+    static boolean[] isEurekaNumber = new boolean[1001];
+
+    private static void preprocess() {
+        // K보다 작은 삼각수 구하기
+        int[] triNums = new int[50];
+        int triNumCount = 0;
+        for (int i = 1; ; i++) {
+            int triNum = i * (i + 1) / 2;
+            if (triNum >= 1000) break;
+            triNums[triNumCount++] = triNum;
+        }
+
+        // isEurekaNumber인지
+        boolean[] isSumOfTriNum = new boolean[1000];
+        for (int i = 0; i < triNumCount; i++) {
+            for (int j = 0; j < triNumCount; j++) {
+                int sum = triNums[i] + triNums[j];
+                if (sum < 1000) isSumOfTriNum[sum] = true;
+            }
+        }
+        for (int i = 1; i < 1000; i++) {
+            if (!isSumOfTriNum[i]) continue;
+            for (int j = 0; j < triNumCount; j++) {
+                int sum = i + triNums[j];
+                if (sum <= 1000) isEurekaNumber[sum] = true;
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        preprocess();
+        
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        
-        for (int i = 0; i < T; i++) {
-            int answer = 0;
-            int K = sc.nextInt();
-            List<Integer> triNums = new ArrayList<>();
-            int n = 1;
-            while ((n * (n + 1)/2) < K) {
-                triNums.add(n * (n + 1) / 2);
-                n++;
-            }
-            for (int a = 0; a < triNums.size(); a++) {
-                int numA = triNums.get(a);
-                for (int b = 0; b < triNums.size(); b++) {
-                    int numB = triNums.get(b);
-                    for (int c = 0; c < triNums.size(); c++) {
-                        int numC = triNums.get(c);
-                        if (numA + numB + numC == K) {
-                            answer = 1;
-                            break;
-                        }
-                    }
-                }
-            }
 
-            System.out.println(answer);
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            System.out.println(isEurekaNumber[K] ? "1" : "0");
         }
     }
 }
