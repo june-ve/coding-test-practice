2484d27894d15b394455bad789d7652563e78d6f
방혜준 <kkngrs2@gmail.com>
2025-11-22 18:12:25 +0900

    [Silver V] Title: 회문인 수, Time: 216 ms, Memory: 18460 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -1,13 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Main {
-
-    static char[] chars = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    , 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q'
-    , 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
-    , 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'
-    , 'z', '+', '/'};
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
@@ -17,17 +12,15 @@ class Main {
             boolean printed = false;
             for (int B = 2; B <= 64; B++) {
                 boolean isYes = true;  // 회문인 수임
-                String convertedNumber = "";
+                List<Integer> convertedNumber = new ArrayList<>();
                 int tmpInputNumber = inputNumber;
                 while (tmpInputNumber > 0) {
-                    convertedNumber = chars[tmpInputNumber % B] + convertedNumber;
+                    convertedNumber.add(0, tmpInputNumber % B);
                     tmpInputNumber /= B;
                 }
-//                System.out.printf("%d진법 : %s%n", B, convertedNumber);
-                for (int i = 0; i < convertedNumber.length() / 2; i++) {
-                    int pairIndex = convertedNumber.length() - i - 1;
-//                    System.out.printf("i = %d, pairI = %d%n", i, pairIndex);
-                    if (convertedNumber.charAt(i) != convertedNumber.charAt(pairIndex)) {
+                for (int i = 0; i < convertedNumber.size() / 2; i++) {
+                    int pairIndex = convertedNumber.size() - i - 1;
+                    if (!convertedNumber.get(i).equals(convertedNumber.get(pairIndex))) {
                         isYes = false;
                         break;
                     }
