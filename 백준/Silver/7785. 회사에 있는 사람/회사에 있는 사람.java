4d9ac3fb995560c35d843b9df365734a3d95a54b
방혜준 <kkngrs2@gmail.com>
2025-12-03 15:27:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1572 ms, Memory: 105504 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[][] records = new String[N][2];
+
+        for (int i = 0; i < N; i++) {
+            records[i][0] = sc.next();
+            records[i][1] = sc.next();
+        }
+
+        Arrays.sort(records, (o1, o2) -> o2[0].compareTo(o1[0]));
+
+        for (int i = 0; i < N - 1; i++) {
+            if (records[i][1].equals("enter") && !records[i][0].equals(records[i + 1][0])) {
+                System.out.println(records[i][0]);
+            }
+        }
+        if (records[N - 1][1].equals("enter"))
+            System.out.println(records[N - 1][0]);
+    }
+}
