984a2ea8333ef57dba163fab087df2977aee9b5e
방혜준 <kkngrs2@gmail.com>
2025-12-03 18:13:13 +0900

    [Silver IV] Title: 문자열 집합, Time: 904 ms, Memory: 67972 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,6 +1,5 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
 
 class Main {
     public static void main(String[] args) {
@@ -8,16 +7,35 @@ class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        Set<String> S = new TreeSet<>();
-        while (N-- > 0) {
-            S.add(sc.next());
+        String[] S = new String[N];
+        for (int i = 0; i < N; i++) {
+            S[i] = sc.next();
         }
+        Arrays.sort(S);
 
         int count = 0;
         while (M-- > 0) {
-            if (S.contains(sc.next()))
+            if (contains(S, sc.next())) {
                 count++;
+            }
         }
         System.out.println(count);
+    }
+
+    private static boolean contains(String[] S, String wordToFind) {
+        int l = 0;
+        int r = S.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (S[m].compareTo(wordToFind) > 0) {
+                r = m - 1;
+            } else if (S[m].compareTo(wordToFind) < 0) {
+                l = m + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
